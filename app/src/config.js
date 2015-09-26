44cda0d90edbbb924f792ca9319502c36c2760a2
Abhinav Bhatele <bhatele@llnl.gov>
2015-09-25 17:17:00 -0700

    use a diverging colormap and add BuYlRd

--- app/src/config.js
+++ app/src/config.js
@@ -1,15 +1,20 @@
 /**
  * Created by yarden on 4/2/15.
  */
+
+var newcolormaps = {BuYlRd: {
+9: ["#4575b4", "#74add1", "#abd9e9", "#e0f3f8", "#ffffbf", "#fee090", "#fdae61", "#f46d43", "#d73027"]
+}};
+
 define(function(require) {
 
   var d3 = require('d3');
 
   var value_band = d3.scale.linear().rangeRound([8, 0]);
-  var value_color = d3.scale.ordinal().domain([0, 8]).range(colorbrewer.YlOrRd[9]);
-  var scale = d3.scale.quantize().range(colorbrewer.YlOrRd[9]);
+  var VALUES_COLORMAP = newcolormaps.BuYlRd[9];
   var JOBS_COLORMAP = colorbrewer.Set1[8].concat();
-  var VALUES_COLORMAP = colorbrewer.YlOrRd[9];
+  var value_color = d3.scale.ordinal().domain([0, 8]).range(VALUES_COLORMAP);
+  var scale = d3.scale.quantize().range(VALUES_COLORMAP);
 
   return {
     MULTI_JOBS_COLOR: '#00ffff',
