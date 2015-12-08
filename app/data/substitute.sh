c49b72f67ac0b32ee58957469fead73fbec93364
Yarden Livnat <yarden@sci.utah.edu>
2015-12-08 10:36:32 -0700

    fix a typo

--- app/data/substitute.sh
+++ app/data/substitute.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find . -name "links/*.csv" | sed \
+find links/ -name "*.csv" | sed \
   -e 's/\(.*\/\)\([^\/]*\)/pushd \1; ln -s \2 \2Z; popd/'  \
   -e 's/links-\([^\.]*\)[^ ]*\(\-.\)\.csvZ/links-\1\2.csv/' \
   -e 's/links-\([^\.]*\)[^ ]*\.csvZ/links-\1.csv/' \
